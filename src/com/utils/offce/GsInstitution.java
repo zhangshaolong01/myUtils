6edd25c45c0b35b4943ff8d9ac0e47ee43c5fcd0
zhangshaolong <zhangshaolong@zhangshaolong.bjgoodwill.com>
2019-08-28 20:10:26 +0800

    offce

---
+++ src/com/utils/offce/GsInstitution.java
@@ -0,0 +1,42 @@
+package com.utils.offce;
+
+/**
+ * 员工账户
+ * 
+ * @author zhangshaolong
+ *
+ */
+public class GsInstitution {
+
+	/**
+	 * 机构编码
+	 */
+	private String institutionId;
+
+	/**
+	 * 机构名称
+	 */
+	private String institutionName;
+
+	public String getInstitutionId() {
+		return institutionId;
+	}
+
+	public void setInstitutionId(String institutionId) {
+		this.institutionId = institutionId;
+	}
+
+	public String getInstitutionName() {
+		return institutionName;
+	}
+
+	public void setInstitutionName(String institutionName) {
+		this.institutionName = institutionName;
+	}
+
+	@Override
+	public String toString() {
+		return "GsInstitution [institutionId=" + institutionId + ", institutionName=" + institutionName + "]";
+	}
+
+}
